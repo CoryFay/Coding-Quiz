d56a9de01c98cbf2b743a4b719395f033132f420
Cory <coryfay97@gmail.com>
2020-10-29 16:45:36 -0700

    add timer and position in html, .css attributes.

--- script.js
+++ script.js
@@ -10,15 +10,50 @@ $(document).ready(function() {
 $("#start").on("click", function() {
     var intro = document.getElementById("main");
     var startButton = document.getElementById("start");
+    //hides beginning page intro and start button
     if (intro.style.display === "block" && startButton.style.display === "block") {
         intro.style.display = "none";
         startButton.style.display = "none";
+        timer.style.display= "block";
     } else {
         intro.style.display = "block";
         startButton.style.display = "block";
+        timer.style.display = "none";
     }
+    //calls timer into action
+    var ninetySeconds = 60 * 1.5,
+    display = document.querySelector('#time');
+    startTimer(ninetySeconds, display);
+    
 
     });
+
+    function startTimer(duration, display) {
+        var timer = duration; //seconds;
+        setInterval(function () {
+            //minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 90, 10);
+    
+            //minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = seconds;
+    
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+    }
+    
+   /* window.onload = function () {
+        var ninetySeconds = 60 * 1.5,
+            display = document.querySelector('#time');
+        startTimer(ninetySeconds, display);
+    };*/
+    
+    
+    
+    
 
 });
 
